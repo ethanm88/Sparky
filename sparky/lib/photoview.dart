bdfdfadb9847caaad3f83e4304821ddf9b160df7
abhi-chau-gt4513 <achauhan63@gatech.edu>
2020-10-17 20:58:03 -0400

    Experiment

---
+++ sparky/lib/photoview.dart
@@ -0,0 +1,18 @@
+import 'package:photo_view/photo_view.dart';
+import 'package:flutter/material.dart';
+
+class Photoview extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Simple Photo View"),
+      ),
+      body: Photoview(
+        // imageProvider: NetworkImage(
+        //     'https://bezkoder.com/wp-content/uploads/2019/07/dart-list-complete-reference-feature-image.png',
+        // ),
+      ),
+    );
+  }
+}
